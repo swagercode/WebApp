5dbd07c60210016a461672df01a1155ac9a0eae3
luke <lukeburrough26@gmail.com>
2025-06-06 15:48:25 -0700

    Consolidated global styles to layout page. Started groups page

--- frontend/src/routes/groups/+page.svelte
+++ frontend/src/routes/groups/+page.svelte
@@ -2,9 +2,6 @@
     import Homenav from '../../components/common/Homenav.svelte';
     import { onMount } from 'svelte';
 
-    let header: Homenav;
-    let offsetHeight = $state(0);
-    let mainWrapper: HTMLElement;
 
     let groups = $state([
         { name: 'Group 1' },
@@ -12,7 +9,14 @@
         { name: 'Group 3' },
     ]);
 
+    let header: Homenav;
+    let lastScrollY = $state(0);
+    let offsetHeight = $state(0);
+    let mainWrapper: HTMLElement;
+    let headerWrapper: HTMLElement;
+
     onMount(() => {
+        // Use requestAnimationFrame to ensure DOM is fully rendered
         requestAnimationFrame(() => {
             if (header) {
                 const height = header.getOffsetHeight();
@@ -22,24 +26,34 @@
                         mainWrapper.style.marginTop = `calc(${offsetHeight}px + 1rem)`;
                     }
                 }
-                
-                window.addEventListener('resize', () => {
-                    const height = header.getOffsetHeight();
-                    if (height !== null && height !== undefined) {
-                        offsetHeight = height;
-                        if (mainWrapper) {
-                            mainWrapper.style.marginTop = `calc(${offsetHeight}px + 1rem)`;
-                        }
-                    }
-                });
             }
         });
+        
+        window.addEventListener('resize', () => {
+            if (header) {
+                const height = header.getOffsetHeight();
+                if (height !== null && height !== undefined) {
+                    offsetHeight = height;
+                    if (mainWrapper) {
+                        mainWrapper.style.marginTop = `calc(${offsetHeight}px + 1rem)`;
+                    }
+                }
+            }
+        });
+
+        window.addEventListener('scroll', () => {
+            if ((window.scrollY === 0 && lastScrollY !== 0) || (window.scrollY !== 0 && lastScrollY === 0)) {
+                header.toggleActive();
+            }
+            lastScrollY = window.scrollY;
+        });
     });
+
 </script>
 
 <div class="page-wrapper">
-    <div class="head-wrapper">
-        <Homenav transition={true} bind:this={header}/>
+    <div class="head-wrapper" bind:this={headerWrapper}>
+        <Homenav bind:this={header}/>
     </div>
 
     <div class="main-wrapper" style="margin-top: calc({offsetHeight}px + 1rem);" bind:this={mainWrapper}>
@@ -50,3 +64,25 @@
         {/each}
     </div>
 </div>
+
+
+
+<style>
+    .page-wrapper {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .head-wrapper {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
