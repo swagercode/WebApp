78afd8a0e7e112054ca46de1c98d7bef86a6470b
swagercode <lukeburrough26@gmail.com>
2025-06-08 13:11:47 -0700

    Removed header component from map so it just has the one from the layout

--- frontend/src/routes/map/+page.svelte
+++ frontend/src/routes/map/+page.svelte
@@ -1,48 +1,12 @@
 <script lang="ts">
     import SpotsMap from '../../components/maps/SpotsMap.svelte';
-    import Homenav from '../../components/common/Homenav.svelte';
 
-    import { onMount } from 'svelte';
-    
-    let header: Homenav;
-    let offsetHeight = $state(0);
-    let mapWrapper: HTMLElement;
+</script>
 
-    onMount(() => {
-        requestAnimationFrame(() => {
-            if (header) {
-                const height = header.getOffsetHeight();
-                if (height !== null && height !== undefined) {
-                    offsetHeight = height;
-                    if (mapWrapper) {
-                        mapWrapper.style.marginTop = `calc(${offsetHeight}px + 1rem)`;
-                    }
-                }
-                
-                window.addEventListener('resize', () => {
-                    const height = header.getOffsetHeight();
-                    if (height !== null && height !== undefined) {
-                        offsetHeight = height;
-                        if (mapWrapper) {
-                            mapWrapper.style.marginTop = `calc(${offsetHeight}px + 1rem)`;
-                        }
-                    }
-                });
-            }
-        });
-    });
 
-    </script>
-
-<div class="page-wrapper">
-    <div class="head-wrapper">
-        <Homenav transition={true} bind:this={header}/>
-    </div>
-
-    <div class="map-wrapper" style="margin-top: calc({offsetHeight}px + 1rem);" bind:this={mapWrapper}>
+    <div class="map-wrapper">
         <SpotsMap />
     </div>
-</div>
 
 <style>
  
