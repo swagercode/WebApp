369743df79c0e611eedfb01d161ea20c496d40dd
swagercode <lukeburrough26@gmail.com>
2025-06-11 01:50:49 -0700

    maybe?????????????????????

--- src/components/spots/searchGallery/SpotComponent.svelte
+++ src/components/spots/searchGallery/SpotComponent.svelte
@@ -20,111 +20,84 @@
                 return String(spot.rating.overall);
         }
     }
-
-    function navigateToSpot() {
-        goto(`/spots/${spot.id}`);
-    }
-
 </script>
 
-<button class="spot" onclick={navigateToSpot}>
-    <div class="spot-image">
-        <img src={spot.images[0]} alt="spot" aria-label={spot.name}/>
-    </div>
-    <div class="spot-info">
-        <div class="spot-top-info">
-            <span class="spot-name-text">{spot.name}</span>
-            <span class="spot-category">{getCurrCategory()}<span class="spot-category-star">★</span></span>
-        </div>
-        <p class="spot-distance">{spot.distance}</p>
-        <p class="spot-open-status" style={spot.openStatus ? "color: var(--open-clr)" : "color: var(--closed-clr)"}>{spot.openStatus ? "Open now" : "Closed"}</p>
-    </div>
-</button>
+
+<a href={`/spots/${spot.id}`}>
+    <article>
+        <figure>
+            <img src={spot.images[0]} alt={spot.name} />
+        </figure>
+        <dl>
+            <dt>
+                <span>{spot.name}</span><span>{getCurrCategory()}<span style="color: var(--rating-clr)">★</span></span>
+            </dt>
+            <dd>
+                {spot.distance}
+            </dd>
+            <dd style="color: var(--{spot.openStatus ? "open-clr" : "closed-clr"})">
+                {spot.openStatus ? "Open now" : "Closed"}
+            </dd>
+        </dl>
+    </article>
+</a>
 
 <style>
-    .spot {       
-        width: 100%;        
-        height: 100%;        
-        display: flex;        
-        flex-direction: column;        
-        cursor: pointer;        
-        border: none;        
-        background: transparent;        
-        padding: 0;        
-        text-align: left;  
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    a {
+        text-decoration: none;
+        color: var(--font-clr-dark) !important;
     }
 
-    .spot:hover {
-        transform: translateY(-2px);
-    }
-
-    .spot-image {
-        aspect-ratio: 1/1;
-    }
-
-    .spot-image img {
+    article {
         width: 100%;
         height: 100%;
-        object-fit: cover;
-        border-radius: 2rem;
-        aspect-ratio: 1/1;
-        width: 100%;
-    }
-
-    .spot-info {
         display: flex;
         flex-direction: column;
-        padding: 0;
-        margin: 0;
-        margin-top: 0.5rem;
+        justify-content: center;
+        align-items: center;
     }
 
-    .spot-top-info {
+    article figure {
+        width: 100%;
+        height: 100%;
+    }
+
+    article figure img {
+        width: 100%;
+        aspect-ratio: 1/1;
+        border-radius: 2rem;
+    }
+
+    article dl {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+    }
+
+    article dt {
+        width: 100%;
         display: flex;
         flex-direction: row;
+        align-items: center;
         justify-content: space-between;
-        width: 100%;
         font-size: 1.5rem;
         font-weight: 300;
         color: var(--font-clr-dark) !important;
-        margin: 0;
-        padding: 0;
     }
 
-    .spot-name-text {
-        font-size: 1.5rem;
-        font-weight: 300;
-        margin: 0;
-        padding: 0;
-        text-align: left;
-        color: var(--font-clr-dark) !important;
-    }
-
-    .spot-distance {
-        font-size: 1rem;
-        font-weight: 100;
-        color: var(--font-clr);
-        margin: 0;
-        padding: 0;
-    }
-
-    .spot-open-status {
-        font-size: 1rem;
-        font-weight: 100;
-        margin: 0;
-        padding: 0;
-    }
-
-    .spot-category {
+    article dt span {
         display: inline-block;
-        text-align: right;
     }
 
-    .spot-category-star {
-        font-size: 1.5rem;
-        color: var(--rating-clr);
-        display: inline-block;
-        text-align: right;
+    article dd {
+        font-size: 0.8rem;
+        color: var(--font-clr-dark);
     }
 </style>
-
