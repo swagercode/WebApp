bf932b50e8da84a3d8aeda29b6f5eec1c4f1bc79
luke <lukeburrough26@gmail.com>
2026-02-19 01:43:34 -0800

    more of the same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_from_directory, request, g, jsonify
 import sqlite3
+import os
 
 app = Flask('__name__')
 
@@ -46,13 +47,17 @@ def spot():
 @app.route('/api/add_spot', methods=['POST'])
 def add_spot():
     data: dict = request.get_json()
+
     REQUIRED_KEYS: set[str] = {'name', 'description', 'address', 'hours', 'phone', 'rating', 'tags'}
+
     if not data or set(data.keys()) != REQUIRED_KEYS:
         return jsonify({'error': 'Please fill all fields'}), 400
+
     g.cur.execute('''
         INSERT INTO spots (name, description, address, hours, phone, rating, tags) VALUES (?, ?, ?, ?, ?, ?, ?);
     ''', (data['name'], data['description'], data['address'], data['hours'], data['phone'], data['rating'], data['tags']))
     g.db.commit()
+
     return jsonify({'success': 'Spot added'})
 
 app.run()
