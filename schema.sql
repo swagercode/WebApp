40fa78c13480595ee796ad4a9d0e9d325a308ea7
luke <lukeburrough26@gmail.com>
2026-02-24 22:09:29 -0800

    full text search in schema

--- schema.sql
+++ schema.sql
@@ -9,3 +9,26 @@ CREATE TABLE spots (
     tags TEXT NOT NULL,
     pictures TEXT NOT NULL
 );
+
+CREATE VIRTUAL TABLE spots_fts USING fts5(
+    name,
+    description,
+    address,
+    tags,
+    content
+)
+
+CREATE TRIGGER spots_ai AFTER INSERT ON spots BEGIN
+    INSERT INTO spots_fts(rowid, name, description, address, tags)
+    VALUES (new.id, new.name, new.description, new.address, new.tags);
+END;
+CREATE TRIGGER spots_ad AFTER DELETE ON spots BEGIN
+    INSERT INTO spots_fts(rowid, name, description, address, tags)
+    VALUES ('delete', old.id, old.name, old.description, old.address, old.tags);
+END;
+CREATE TRIGGER spots_ai AFTER UPDATE ON spots BEGIN
+    INSERT INTO spots_fts(rowid, name, description, address, tags)
+    VALUES ('delete', old.id, old.name, old.description, old.address, old.tags);
+    INSERT INTO spots_fts(rowid, name, description, address, tags)
+    VALUES (new.id, new.name, new.description, new.address, new.tags);
+END;
